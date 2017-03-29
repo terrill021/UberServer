5871c244ef4f038a32b30f790f451fb99a58532b
Alejandro Terrill D <terrill021@hotmail.com>
2017-03-29 17:47:30 -0500

    after blue screem

--- app/models/client.server.model.js
+++ app/models/client.server.model.js
@@ -5,6 +5,7 @@ var ClientsSchema = new Schema({
 	name : String,
 	user: {type : String, trim : true, unique : true},
 	password : String,
+	phone : String,
 	email : String,
 	balance : Number, 
 	creationDate : Date
